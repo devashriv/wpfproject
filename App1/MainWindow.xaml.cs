328e70b939db0fb3a47522e6cba6d6f67c5f9e99
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-04 08:31:35 -0500

    initial commit

---
+++ App1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "joe", LastName = "Smith" });
+            people.Add(new Person { FirstName = "Elli", LastName = "Jackson" });
+
+            myComboBox.ItemsSource = people;
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"First name is {firstNameText.Text}");
+        }
+    }
+}
---
+++ App1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
